fc62437382e21c5c54f1db7db56b072a1c50b8d1
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-11 22:03:47 -0700

    Turtle1.py

---
+++ Turtle1.py
@@ -0,0 +1,11 @@
+import turtle             #first we have to import turtle to our python
+sparsh = turtle.Turtle()  #here we have assigned sparsh variable to turtle 
+                          #so we dont have to type turtle again we can use sparsh you may can type anything
+
+  sparsh.forward(100)     #here we have moved our turtle to forward by 100 positions
+  sparsh.right(90)        #here we are aiming to change the position of turtle to right by 90 degrees
+  sparsh.forward(100)
+  sparsh.right(90)
+  sparsh.forward(100)
+  sparsh.right(90)
+  sparsh.forward(100)
